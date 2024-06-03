bacc3b61a4bf04324eca513fab7891262666edaf
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2024-06-03 11:43:51 -0300

    feat: Sidebar

---
+++ src/components/Sidebar/Profile.tsx
@@ -0,0 +1,23 @@
+import { LogOut } from 'lucide-react'
+
+export function Profile() {
+  return (
+    <div className="flex items-center gap-3">
+      <img
+        src="https://github.com/SchunckLeonardo.png"
+        className="h-10 w-10 rounded-full"
+        alt=""
+      />
+      <div className="flex flex-1 flex-col truncate">
+        <span className="text-sm font-semibold">Leonardo Schunck</span>
+        <span className="text-sm text-zinc-500">leo@gmail.com</span>
+      </div>
+      <button
+        type="button"
+        className="ml-auto rounded-md p-2 hover:bg-zinc-100"
+      >
+        <LogOut className="h-5 w-5 text-zinc-500" />
+      </button>
+    </div>
+  )
+}
