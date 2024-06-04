da2e2bfd6b48902c3399dfef0ac8d571d1cb99da
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2024-06-04 14:34:38 -0300

    feat: dark mode complete

--- src/components/Form/SelectInput/index.tsx
+++ src/components/Form/SelectInput/index.tsx
@@ -16,7 +16,7 @@ export function SelectInput({
 }: SelectInputProps) {
   return (
     <Select.Root {...props}>
-      <Select.Trigger className="flex h-11 w-full items-center justify-between gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm outline-none focus:border-violet-300 focus:ring-2 focus:ring-violet-100 data-[placeholder]:text-zinc-600">
+      <Select.Trigger className="flex h-11 w-full items-center justify-between gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm outline-none focus:border-violet-300 focus:ring-2 focus:ring-violet-100 data-[placeholder]:text-zinc-600 dark:border-zinc-700 dark:bg-zinc-800 dark:focus-within:border-violet-500 dark:focus-within:ring-violet-500/10 dark:data-[placeholder]:text-zinc-400">
         <Select.Value placeholder={placeholder} className="text-black" />
         <Select.Icon>
           <ChevronDown className="h-5 w-5 text-zinc-500" />
@@ -27,7 +27,7 @@ export function SelectInput({
         <Select.Content
           side="bottom"
           position="popper"
-          className="animate-slideDownAndFade z-10 w-[--radix-select-trigger-width] rounded-lg border border-zinc-200 bg-white shadow-sm"
+          className="animate-slideDownAndFade z-10 w-[--radix-select-trigger-width] rounded-lg border border-zinc-200 bg-white shadow-sm dark:border-zinc-700 dark:bg-zinc-800"
           sideOffset={8}
         >
           <Select.Viewport>{children}</Select.Viewport>
