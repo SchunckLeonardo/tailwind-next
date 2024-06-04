cead766c34c707fdd98cf731c667d75afd616d36
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2024-06-04 12:25:37 -0300

    feat: improve acessibility

--- src/components/Form/SelectInput/index.tsx
+++ src/components/Form/SelectInput/index.tsx
@@ -16,7 +16,7 @@ export function SelectInput({
 }: SelectInputProps) {
   return (
     <Select.Root {...props}>
-      <Select.Trigger className="flex h-11 w-full items-center justify-between gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm outline-none data-[placeholder]:text-zinc-600">
+      <Select.Trigger className="flex h-11 w-full items-center justify-between gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm outline-none focus:border-violet-300 focus:ring-2 focus:ring-violet-100 data-[placeholder]:text-zinc-600">
         <Select.Value placeholder={placeholder} className="text-black" />
         <Select.Icon>
           <ChevronDown className="h-5 w-5 text-zinc-500" />
